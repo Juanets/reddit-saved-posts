16016d86cef4a0bd935b91da88025d275278d5af
Juanets <jdgs.gt@gmail.com>
2016-11-30 17:44:18 -0700

    views and functions for oauth

--- app/views.py
+++ app/views.py
@@ -1,11 +1,28 @@
-from app.reddit import login, get_posts, group_by_subreddit
-from flask import render_template
+from app.reddit import oauth, get_posts, group_by_subreddit, set_user
+from flask import render_template, url_for, redirect, request
 from app import app
 
 
 @app.route('/')
 def index():
-	login()
+	return render_template('index.html')
+
+
+@app.route('/oauth')
+def authentication():
+	return redirect(oauth())
+
+
+@app.route('/oauth_callback')
+def oauth_callback():
+	code = request.args.get('code')
+	set_user(code)
+
+	return redirect(url_for('get_saved'))
+
+
+@app.route('/saved')
+def get_saved():
 	saved = group_by_subreddit(get_posts())
 	return render_template('index.html', saved=saved)
 
